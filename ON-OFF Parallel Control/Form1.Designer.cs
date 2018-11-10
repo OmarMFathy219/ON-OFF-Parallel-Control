e70204987642673f33bf4544b7aa86aef0d15951
Omar M. Fathy <OmarMFathy25@gmail.com>
2018-11-11 00:13:31 +0200

    ON-OFF Parallel Control

---
+++ ON-OFF Parallel Control/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ON_OFF_Parallel_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(139, 96);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(124, 23);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "ON";
+            this.Button1.UseSelectable = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 176);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.Text = "Controlling An Electrical Appliance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton Button1;
+    }
+}
+
---
+++ ON-OFF Parallel Control/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ON_OFF_Parallel_Control
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        class PortInterop
+        {
+            [DllImport("inpout32.dll", EntryPoint = "out32")]
+            public static extern void Output(int address, int value);
+
+            [DllImport("inpout32.dll", EntryPoint = "Inp32")]
+            public static extern int Input(int address);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int input;
+            input = PortInterop.Input(889);
+            if (input == 225)
+            {
+                Button1.Text = "ON";
+            }
+            else
+            { 
+                Button1.Text = "OFF";
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(Button1.Text=="OFF")
+            {
+                //Turn The Circuit ON
+                PortInterop.Output(888, 255);
+                Button1.Text = "ON";
+            }
+            else
+            {
+                //Turn The Circuit OFF
+                PortInterop.Output(888, 0);
+                Button1.Text = "OFF";
+            }
+        }
+    }
+}
